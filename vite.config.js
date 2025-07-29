2640af004120ca189adf0e51a70feca38907189a
anantinfinity <ganant407@gmail.com>
2025-07-29 14:08:27 +0530

    finished

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
